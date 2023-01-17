aa13f841674c0a04e9a82b832cc1f361987f3aa4
SHJ123 <stijnnouws@hotmail.nl>
2023-01-17 09:50:09 +0100

    adding some basic analysis

--- real/main.py
+++ real/main.py
@@ -21,11 +21,28 @@ def open_file(input):
 if __name__ == '__main__':
 
 
-
+    number_of_moves = []
 
     # --------------------------- Random movement --------------------------
-    for i in range(20):
+    for i in range(1000):
         car_list_1 = open_file('data/Rushhour6x6_2.csv')
         test = RandomAlgorithm(car_list_1, 6)
         test.main_random(100000)
+        number_of_moves.append(len(test.made_moves))
     print(test.made_moves)
+
+    #perform analysis
+    import matplotlib.pyplot as plt
+    #make the average
+    average = sum(number_of_moves)/len(number_of_moves)
+    max = max(number_of_moves)
+    min = min(number_of_moves)
+    print(f"the average number of moves is {average}, the maximum total moves is {max} and the least number of moves is {min}")
+
+    # plot the values on a graph
+    plt.hist(number_of_moves)
+    plt.title("distribution of means")
+    plt.xlabel("the number of moves until it's in the winning position")
+    plt.ylabel("the number of simuilations at a given position")
+
+    plt.show()
